7d357f99e7788d363894441ca90e4851ca7db63d
Kyle Kemp <kyle@seiyria.com>
2024-04-24 11:00:02 -0500

    actually add component

---
+++ src/app/_shared/components/card-text/card-text.component.ts
@@ -0,0 +1,41 @@
+import { Component, effect, input } from '@angular/core';
+import { marked } from 'marked';
+import { environment } from '../../../../environments/environment';
+
+@Component({
+  selector: 'app-card-text',
+  templateUrl: './card-text.component.html',
+  styleUrls: ['./card-text.component.scss'],
+})
+export class CardTextComponent {
+  public text = input.required<string>();
+
+  public html = '';
+
+  constructor() {
+    const renderer = this.getCustomRenderer();
+    effect(() => {
+      this.html = marked(this.text(), { renderer });
+    });
+  }
+
+  private getCustomRenderer(): marked.Renderer {
+    const renderer = new marked.Renderer();
+
+    // custom inline image formatter
+    renderer.codespan = (text: string) => {
+      if (text.includes(':')) {
+        const [type, subtype] = text.split(':');
+
+        return `<img src="${environment.baseUrl}/symbols/${type}-${subtype}.webp" class="inline-icon" title="${subtype}" />`;
+      }
+
+      return `<pre>${text}</pre>`;
+    };
+
+    // no paragraphs
+    renderer.paragraph = (text: string) => text;
+
+    return renderer;
+  }
+}
