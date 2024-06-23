2af222ccce83665962caa02565631a32ce00835a
Kyle Kemp <kyle@seiyria.com>
2024-06-23 10:57:17 -0500

    work on #93 to allow for &setlocale

--- src/app/locale.service.ts
+++ src/app/locale.service.ts
@@ -1,11 +1,13 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { LocalStorageService } from 'ngx-webstorage';
 import { environment } from '../environments/environment';
+import { WINDOW } from './_shared/helpers';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LocaleService {
+  private window = inject(WINDOW);
   private localStorageService = inject(LocalStorageService);
 
   private locale = signal<string>('en-US');
@@ -24,6 +26,12 @@ export class LocaleService {
 
   public async init() {
     this.locale.set(this.localStorageService.retrieve('locale') ?? 'en-US');
+
+    const urlParams = new URLSearchParams(this.window.location.search);
+    const setLocaleFromURL = urlParams.get('setlocale');
+    if (setLocaleFromURL) {
+      this.locale.set(setLocaleFromURL);
+    }
   }
 
   public setLocales(locales: string[]) {
