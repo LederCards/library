f26f5ce29eefd6b783c50860de5f64bdc1af41b1
Kyle Kemp <kyle@seiyria.com>
2024-06-26 15:07:02 -0500

    closes #99

--- src/app/_shared/components/card-display/card-display.component.ts
+++ src/app/_shared/components/card-display/card-display.component.ts
@@ -1,6 +1,12 @@
-import { Component, effect, inject, input } from '@angular/core';
-import { type ICard } from '../../../../../interfaces';
+import { Component, computed, inject, input, type Signal } from '@angular/core';
+import {
+  type ICard,
+  type ICardErrataEntry,
+  type ICardFAQEntry,
+} from '../../../../../interfaces';
 import { CardsService } from '../../../cards.service';
+import { ErrataService } from '../../../errata.service';
+import { FAQService } from '../../../faq.service';
 
 type CardSize = 'grid' | 'small' | 'normal' | 'large';
 type CardDisplay = 'images' | 'text';
@@ -12,23 +18,41 @@ type CardDisplay = 'images' | 'text';
 })
 export class CardDisplayComponent {
   private cardsService = inject(CardsService);
+  private faqService = inject(FAQService);
+  private errataService = inject(ErrataService);
 
   public size = input<CardSize>('normal');
   public cardCode = input.required<string>();
   public display = input<CardDisplay>('images');
 
-  public card: ICard | undefined;
-  public soulArray = [];
+  public displayText = true;
 
-  public get altText() {
-    if (!this.card) return '';
-
-    return `${this.card.id} - ${this.card.name} - card image display`;
+  public get queryString() {
+    return new URLSearchParams(window.location.search).get('q');
   }
 
-  constructor() {
-    effect(() => {
-      this.card = this.cardsService.getCardById(this.cardCode());
-    });
-  }
+  public faq: Signal<ICardFAQEntry[]> = computed(() => {
+    const cardData = this.cardData();
+    if (!cardData) return [];
+
+    return this.faqService.getCardFAQ(cardData.game, cardData.name);
+  });
+
+  public errata: Signal<ICardErrataEntry[]> = computed(() => {
+    const cardData = this.cardData();
+    if (!cardData) return [];
+
+    return this.errataService.getCardErrata(cardData.game, cardData.name);
+  });
+
+  public cardData: Signal<ICard | undefined> = computed(() => {
+    return this.cardsService.getCardById(this.cardCode());
+  });
+
+  public altText: Signal<string> = computed(() => {
+    const card = this.cardData();
+    if (!card) return '';
+
+    return `${card.id} - ${card.name} - card image display`;
+  });
 }
