4e55ac5c70bd5939361cda4ccdc7a73ac6541a42
Kyle J. Kemp <kyle@seiyria.com>
2025-09-11 10:59:51 -0500

    adjust faq pipe to support locale and version

--- src/app/_shared/pipes/faq.pipe.ts
+++ src/app/_shared/pipes/faq.pipe.ts
@@ -13,15 +13,22 @@ export class FaqPipe implements PipeTransform {
     const [productId, cardDisplay] = args;
     if (!productId) return value ?? '';
 
-    const ruleRegex = /\$rule:([\d.]+)\$/gm;
+    const ruleRegex = /\$rule:(.*)\$/gm;
     value = value.replace(ruleRegex, (match, p1) => {
+      const [rule, locale, version] = p1.split(':');
       if (!productId) return p1;
 
       const rulesUrl = this.metaService.getRulesByProductId(productId);
       if (!rulesUrl) return p1;
 
-      return `<a href="${rulesUrl}/#${p1}" target="_blank">${
-        cardDisplay || p1
+      const url = new URL(rulesUrl);
+      url.hash = rule;
+      url.searchParams.set('product', productId);
+      if (locale) url.searchParams.set('locale', locale);
+      if (version) url.searchParams.set('version', version);
+
+      return `<a href="${url.toString()}" target="_blank">${
+        cardDisplay || rule || p1
       }</a>`;
     });
 
